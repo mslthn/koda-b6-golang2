a6e804ccf1f72e86be3c2d075b5ed9161fa33b81
mslthn <msulthon1909@gmail.com>
2026-02-11 14:35:59 +0700

    feat: create program to insert a number to slice

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice()
+	
+	scores := []int{50, 75, 66, 20, 32, 90}
+	newNumber := 88
+	index := 3
+
+	scores = append(scores[:index], append([]int {newNumber}, scores[index:]...)...)
+
+	for i :=range len(scores) {
+		fmt.Println(scores[i])
+	}
+}
