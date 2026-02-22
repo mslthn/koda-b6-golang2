17c3f9fd49760472e8179c243e8eca519c1068db
mslthn <msulthon1909@gmail.com>
2026-02-22 12:34:43 +0700

    fix: delete unsed lines

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import "fmt"
 
 func main() {
 	slice()
-	
+
 	scores := []int{50, 75, 66, 20, 32, 90}
 	newNumber := 88
 	index := 3
-
-	scores = append(scores[:index], append([]int {newNumber}, scores[index:]...)...)
-
-	for i :=range len(scores) {
+	scores = append(scores[:index], append([]int{newNumber}, scores[index:]...)...)
+	
+	for i := range len(scores) {
 		fmt.Println(scores[i])
 	}
 }
